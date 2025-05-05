c658d4336aa247486bf75cf3737d19d91cfc6325
AlphacaDev <79185882+AlphacaDev@users.noreply.github.com>
2025-05-05 00:46:06 -0400

    Home.py

--- alexxie-calc2/Home.py
+++ alexxie-calc2/Home.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.title("Polar Curves")
 
 st.write("This website will display 3 different types of " \
-"polar curves: Rose Curves, Devil's Curves, and ")
+"polar curves: Rose Curves, Devil's Curves, and Cardioids.")
